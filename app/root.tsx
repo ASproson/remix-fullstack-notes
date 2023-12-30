b001bfc32aba93f944648d4fc329dd7c70bce89f
atlas <a.sproson@outlook.com>
2023-12-30 10:09:35 +0000

    Abstracts nav-footer into component, calls Index

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,7 @@ import { Links, LiveReload, Scripts } from "@remix-run/react";
 import faviconAssetUrl from "./assets/favicon.svg";
 import fontStylesStyleSheetUrl from "./styles/font.css";
 import tailwindStyleSheet from "./styles/tailwind.css";
+import { Index } from "./routes";
 
 /**
  * @returns `<link>` tags to be inserted into the `<head>` on route transitions
@@ -22,17 +23,39 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full overflow-x-hidden">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Links />
       </head>
-      <body>
-        <h1 className="p-8 text-xl">Hello world</h1>
+      <body className="flex h-full flex-col justify-between">
+        <header className="container mx-auto py-6">
+          <nav className="flex justify-between">
+            <NavFooter />
+          </nav>
+        </header>
+
+        <div className="flex-1">
+          <Index />
+        </div>
+
+        <footer className="container mx-auto flex justify-between">
+          <NavFooter />
+        </footer>
+        <div className="h-5" />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
 }
+
+const NavFooter = () => {
+  return (
+    <div>
+      <div className="font-light">fullstack</div>
+      <div className="font-bold">notes</div>
+    </div>
+  );
+};
