0a837cb791d41a62271546262833af28a846a171
atlas <a.sproson@outlook.com>
2024-01-01 13:47:39 +0000

    Rebuild

--- app/root.tsx
+++ app/root.tsx
@@ -1,82 +1,47 @@
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Link,
-  Links,
-  LiveReload,
-  Outlet,
-  Scripts,
-  json,
-  useLoaderData,
-} from "@remix-run/react";
-import faviconAssetUrl from "./assets/favicon.svg";
-import fontStylesStyleSheetUrl from "./styles/font.css";
-import tailwindStyleSheet from "./styles/tailwind.css";
-import { getNotes } from "./utils/data";
+import { type LinksFunction } from '@remix-run/node'
+import { Links, LiveReload, Outlet, Scripts } from '@remix-run/react'
+import faviconAssetUrl from './assets/favicon.svg'
+import fontStylestylesheetUrl from './styles/font.css'
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
-/**
- * @returns `<link>` tags to be inserted into the `<head>` on route transitions
- */
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: tailwindStyleSheet },
-  {
-    rel: "icon",
-    type: "image/svg+xml",
-    href: faviconAssetUrl,
-  },
-  {
-    rel: "stylesheet",
-    href: fontStylesStyleSheetUrl,
-  },
-];
-
-export const loader = async () => {
-  const notes = await getNotes();
-  return json({ notes });
-};
-
-export default function App() {
-  const { notes } = useLoaderData<typeof loader>();
-  return (
-    <html lang="en" className="h-full overflow-x-hidden">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Links />
-      </head>
-      <body className="flex h-full flex-col justify-between">
-        <header className="container mx-auto py-6">
-          <nav className="flex justify-between">
-            <NavFooter />
-          </nav>
-        </header>
-
-        <div className="flex-1">
-          <ul>
-            {notes.map((note) => (
-              <li key={note.id}>
-                <Link to={`notes/${note.id}`}>Note {note.id}</Link>
-              </li>
-            ))}
-          </ul>
-          <Outlet />
-        </div>
-
-        <footer className="container mx-auto flex justify-between">
-          <NavFooter />
-        </footer>
-        <div className="h-5" />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+export const links: LinksFunction = () => {
+	return [
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		{ rel: 'stylesheet', href: fontStylestylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
+	]
 }
 
-const NavFooter = () => {
-  return (
-    <Link to={`/`}>
-      <div className="font-light">fullstack</div>
-      <div className="font-bold">notes</div>
-    </Link>
-  );
-};
+export default function App() {
+	return (
+		<html lang="en" className="h-full overflow-x-hidden">
+			<head>
+				<Links />
+			</head>
+			<body className="flex h-full flex-col justify-between bg-background text-foreground">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+				</div>
+				<div className="h-5" />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
+}
--- app/routes/notes.$nodeId.tsx
+++
@@ -1,7 +0,0 @@
-export const Note = () => {
-  return (
-    <div>
-      <h1 className="text-red-500">I AM A NOTE</h1>;
-    </div>
-  );
-};
