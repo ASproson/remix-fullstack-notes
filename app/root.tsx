7bc4373851d29d639a42470eac4d07fc98bc7211
atlas <a.sproson@outlook.com>
2024-01-12 15:10:45 +0000

    Adds error handling for routes

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import faviconAssetUrl from './assets/favicon.svg'
 import fontStylestylesheetUrl from './styles/font.css'
 import tailwindStylesheetUrl from './styles/tailwind.css'
 import { getEnv } from './utils/env.server.ts'
+import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 
 export const links: LinksFunction = () => {
 	return [
@@ -27,48 +28,57 @@ export async function loader() {
 	return json({ ENV: getEnv() })
 }
 
-export default function App() {
-	const data = useLoaderData<typeof loader>()
+// Ensures that if an error occurs at app or one level below that an error is shown
+const Document = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
 				<Meta />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<Links />
 			</head>
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
-				<header className="container mx-auto py-6">
-					<nav className="flex justify-between">
-						<Link to="/">
-							<div className="font-light">fullstack</div>
-							<div className="font-bold">notes</div>
-						</Link>
-					</nav>
-				</header>
-
-				<div className="flex-1">
-					<Outlet />
-				</div>
-
-				<div className="container mx-auto flex justify-between">
-					<Link to="/">
-						<div className="font-light">fullstack</div>
-						<div className="font-bold">notes</div>
-					</Link>
-				</div>
-				<div className="h-5" />
+				{children}
 				<ScrollRestoration />
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
-					}}
-				/>
-
 				<Scripts />
 				<LiveReload />
 			</body>
 		</html>
+	)
+}
+
+export default function App() {
+	const data = useLoaderData<typeof loader>()
+	return (
+		<Document>
+			<header className="container mx-auto py-6">
+				<nav className="flex justify-between">
+					<Link to="/">
+						<div className="font-light">fullstack</div>
+						<div className="font-bold">notes</div>
+					</Link>
+				</nav>
+			</header>
+
+			<div className="flex-1">
+				<Outlet />
+			</div>
+
+			<div className="container mx-auto flex justify-between">
+				<Link to="/">
+					<div className="font-light">fullstack</div>
+					<div className="font-bold">notes</div>
+				</Link>
+			</div>
+			<div className="h-5" />
+			<ScrollRestoration />
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+				}}
+			/>
+		</Document>
 	)
 }
 
@@ -77,4 +87,14 @@ export const meta: MetaFunction = () => {
 		{ title: 'Remix Fullstack Notes' },
 		{ name: 'description', content: `ASproson's portfolio page` },
 	]
+}
+
+export function ErrorBoundary() {
+	return (
+		<div className="flex-1">
+			<Document>
+				<GeneralErrorBoundary />
+			</Document>
+		</div>
+	)
 }
